c01a67d46e4cf50aea0e924176f072da9b4b18bc
KonstantinGusev <konstantinvgusev@gmail.com>
2016-06-05 03:35:09 +0300

    Добавлено разбиение предложений на слова

--- ConsoleApplication42/ConsoleApplication42/Program.cs
+++ ConsoleApplication42/ConsoleApplication42/Program.cs
@@ -21,35 +21,48 @@ namespace ConsoleApplication144
         {
             ///Разделение "Войны и мира на предложения"
             /// 1- этап
+            List<string> words = new List<string>();
             var webClient = new WebClient();
+            
             webClient.Encoding = Encoding.GetEncoding(1251);
             var text = webClient.DownloadString(
                 "http://vojnaimir.ru/files/book1.txt");
 
-            var regex = new Regex("[А-Яа-я0-9+\\.+\\s+\\?+\\!+]+(?!\\?\\!\\.\\s+[А-Я]+)");
-            var words =
+            var regex = new Regex("[A-Z А-Я].*?(?=[.!?])");
+            var sentence =
                 regex.Matches(text)
                     .OfType<Match>()
                     .Select(match => match.Value)
                     .ToList();
 
-            var list = words
-                    .GroupBy(s => s)
-                    .OrderByDescending(g => g.Count())
-                    .Select(g => new
-                    {
-                        Word = g.Key,
-                        P = g.Count() * 1.0 / words.Count
-                    })
-                    .Take(100)
-                    .ToArray();
+            //второй этап разделение предложений на строки
+            var regex2 = new Regex("\\w+?\\b");
+            var list =
+                sentence
+                .OfType<Match>()
+                .Select(match => match.Value)
+                .Select(match => match.Trim())
+                .GroupBy(match => match)
+                .Select(match => match.Key)
+                .ToList();
+            //var list = sentence
+            //        .GroupBy(s => s)
+            //        .OrderByDescending(g => g.Count())
+            //        .Select(g => new
+            //        {
+            //            //            Word = g.Key,
+                        //            P = g.Count() * 1.0 / sentence.Count
+                        //        })
+                        // .Take(100)
+                        //.ToArray();
 
-            foreach (var item in list)
-            {
-                Console.WriteLine("{0:F2}%\t{1}", item.P * 100, item.Word);
-            }
-            Console.ReadKey();
-        }
+                        //foreach (var item in list)
+                        //{
+
+                        //    Console.WriteLine("{0:F2}%\t{1}", item.P * 100, item.Word);
+                        //}
+                        //Console.ReadKey();
+                    }
     }
 }
 
